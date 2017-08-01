11785642a8531be5336302c0aeea0dfe61e35940
Kiran <kiranreddyamk@gmail.com>
2017-08-01 02:50:15 -0400

    Inital Commit

---
+++ src/ge/hsbc/cts/lowes/capitalone/PrintsForEverySecond.java
@@ -0,0 +1,26 @@
+package ge.hsbc.cts.lowes.capitalone;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class PrintsForEverySecond {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+		Timer timer = new Timer();
+
+		timer.schedule(new TimerTask() {
+			int count = 1;
+
+			@Override
+			public void run() {
+				System.out.println("Timer " + count);
+				count++;
+			}
+		}, 0, 1000); //set to 1 sec 60000 is 1 minute
+
+	}
+
+}
